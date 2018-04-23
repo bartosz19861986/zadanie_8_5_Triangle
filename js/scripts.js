d3479e3a4b62d2d0b19058e4c673d7a8dfc62ffe
Bartosz Pluta <barpluta@o2.pl>
2018-04-23 13:26:49 +0200

    Add scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var a = 10;
+var h = 20;
+var triangleArea;
+
+triangleArea = a*h/2;
+
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
